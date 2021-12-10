6c62b414505c6eae7de26d2361f0d83efb3c8094
Shivanshu Misra <shivanshu.misra9@gmail.com>
2021-12-10 16:59:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './App.css'
 
-function App() {
+const App = () => {
+  let time = new Date().toLocaleTimeString('en-US', {hour12: true});
+  const [cTime, setcTime]= useState(time)
+
+  const UpdateTime = () => {
+    //let time2 = new Date().toLocaleTimeString('en-US', {hour12: true});
+  
+    setcTime(time)
+    // console.log(time)
+  }
+  setInterval(UpdateTime, 1000)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{cTime}</h1>
     </div>
   );
 }
